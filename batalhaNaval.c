2eb959fd0f6aae5fef4957d8cb6100a68bf03775
ViNiciusVicTorio1 <161782159+ViNiciusVicTorio1@users.noreply.github.com>
2024-12-21 02:37:54 +0000

    commit nível mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+// Função para exibir o tabuleiro
+void exibirTabuleiro(int tabuleiro[10][10]) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Função para aplicar uma habilidade no tabuleiro
+void aplicarHabilidade(int tabuleiro[10][10], int habilidade[5][5], int x, int y) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (habilidade[i][j] == 1 && x + i < 10 && y + j < 10) {
+                tabuleiro[x + i][y + j] = 1;
+            }
+        }
+    }
+}
+
 int main() {
     // Dimensões do tabuleiro
     int tabuleiro[10][10] = {0};
@@ -30,14 +51,39 @@ int main() {
         tabuleiro[navio_diagonal2_x - i][navio_diagonal2_y + i] = 3;
     }
 
+    // Matrizes para habilidades especiais
+    int cone[5][5] = { 
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    int cruz[5][5] = { 
+        {0, 0, 1, 0, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    int octaedro[5][5] = { 
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    // Aplicando habilidades no tabuleiro
+    aplicarHabilidade(tabuleiro, cone, 1, 1);
+    aplicarHabilidade(tabuleiro, cruz, 4, 4);
+    aplicarHabilidade(tabuleiro, octaedro, 7, 7);
+
     // Exibindo o tabuleiro completo
     printf("Tabuleiro:\n");
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
+    exibirTabuleiro(tabuleiro);
 
     return 0;
 }
